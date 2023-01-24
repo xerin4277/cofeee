34a42269bac4fb82869b53bb1a04c761cdd152e6
xerin4277 <jasperdaquila@gmail.com>
2023-01-24 14:51:19 +0800

    added template

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/homepage', function () {
+    return view('homepage');
 });
 
 Route::get('/dashboard', function () {
